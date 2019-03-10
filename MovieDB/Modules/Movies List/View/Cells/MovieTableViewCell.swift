99765d573697a423ae11811318cd10325fb6db8f
Mohamed Ramadan AbdelHafez <santamo94@gmail.com>
2019-03-11 01:10:53 +0200

    add movie cell

---
+++ MovieDB/Modules/Movies List/View/Cells/MovieTableViewCell.swift
@@ -0,0 +1,44 @@
+//
+//  MovieTableViewCell.swift
+//  MovieDB
+//
+//  Created by Mo-Ramadan Abdelhafez on 3/11/19.
+//  Copyright © 2019 Mo-Ramadan Abdelhafez. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet fileprivate weak var avatarImageView: UIImageView!
+    @IBOutlet fileprivate weak var releaseDateLabel: UILabel!
+    @IBOutlet fileprivate weak var titleLabel: UILabel!
+    @IBOutlet fileprivate weak var overviewLabel: UILabel!
+    
+    var viewModel: MovieViewModel? {
+        didSet {
+            if let viewModel = viewModel {
+                self.configure(viewModel: viewModel)
+            }
+        }
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setupView()
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        viewModel = nil
+    }
+    
+    func setupView() {
+        //TODO: setup rounded views and colors
+    }
+    
+    private func configure(viewModel: MovieViewModel) {
+        //TODO: configure cell
+    }
+    
+}
--- MovieDB/Modules/Movies List/View/MoviesListViewController.swift
+++ MovieDB/Modules/Movies List/View/MoviesListViewController.swift
@@ -10,10 +10,61 @@ import UIKit
 
 class MoviesListViewController: BaseViewController {
 
+    @IBOutlet fileprivate weak var tableView: UITableView!
+    
     var presenter: MoviesListPresenterProtocol?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupView()
+        setupTableView()
+    }
+    
+    private func setupView() {
+        //TODO: setup colors and navbar title ;;; make it largeTitle
+    }
+}
+
+extension MoviesListViewController {
+    
+    var cells: [UITableViewCell.Type] {
+        return [MovieTableViewCell.self]
+    }
+    
+    func setupTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.estimatedRowHeight = 100
+        tableView.rowHeight = UITableView.automaticDimension
+        registerCells()
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+    }
+    
+    func registerCells() {
+        cells.forEach { cellClassName in
+            let cellName = String(describing: cellClassName)
+            let nib = UINib(nibName: cellName, bundle: nil)
+            tableView.register(nib, forCellReuseIdentifier: cellName)
+        }
+    }
+}
+
+extension MoviesListViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        //TODO: later an enum to represent allMovies and myMovies sections
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //TODO: get this from interactor according to your data source
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell  = tableView.dequeueReusableCell(withIdentifier: String(describing: MovieTableViewCell.self)) as? MovieTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
     }
 }
 
