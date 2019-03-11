14969f73af51fe6fad591fc87aa6c39ab308230f
Mohamed Ramadan AbdelHafez <santamo94@gmail.com>
2019-03-11 11:07:35 +0200

    init movie creation view controller

---
+++ MovieDB/Modules/Movie Creation/MovieCreationViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MovieCreationViewController.swift
+//  MovieDB
+//
+//  Created by Mo-Ramadan Abdelhafez on 3/11/19.
+//  Copyright © 2019 Mo-Ramadan Abdelhafez. All rights reserved.
+//
+
+import UIKit
+
+class MovieCreationViewController: BaseViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
