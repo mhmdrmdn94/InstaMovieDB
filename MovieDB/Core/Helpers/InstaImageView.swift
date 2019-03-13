55a9e3427fa635f5bcd8f1b61cbf9aee87d79932
Mohamed Ramadan AbdelHafez <santamo94@gmail.com>
2019-03-13 21:45:39 +0200

    fix images flickering issue

--- MovieDB/Core/Helpers/InstaImageView.swift
+++ MovieDB/Core/Helpers/InstaImageView.swift
@@ -10,7 +10,11 @@ import UIKit
 
 class InstaImageView: UIImageView {
     
+    private var imageUrlString: String?
+    
     func setImage(withUrl url: URL) {
+        
+        self.imageUrlString = url.absoluteString
         
         let cachedImage = InstaImageCache.shared.getImageFromCache(urlString: url.absoluteString)
         if let image = cachedImage {
@@ -20,15 +24,18 @@ class InstaImageView: UIImageView {
         
         let urlSession = URLSession.shared
         urlSession.dataTask(with: url) { [weak self] (data, response, error) in
+            
             guard let strongSelf = self else { return }
             if let error = error {
                 print(error.localizedDescription)
                 return
             }
-            if let imageData = data,
-                let image = UIImage(data: imageData) {
+            
+            if let imageData = data, let image = UIImage(data: imageData) {
                 InstaImageCache.shared.addImageToCache(image: image, urlString: url.absoluteString)
-                strongSelf.setImage(image)
+                if let imageUrlString = strongSelf.imageUrlString, imageUrlString == url.absoluteString {
+                    strongSelf.setImage(image)
+                }
             }
             
         }.resume()
